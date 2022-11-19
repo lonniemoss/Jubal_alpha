8afd4f2a538bc5e73d40371a1b518faef9f881b3
glory jacques <101422810+lonniemoss@users.noreply.github.com>
2022-11-19 17:04:08 -0500

    audio parameter's done

--- PluginProcessor.h
+++ PluginProcessor.h
@@ -55,6 +55,10 @@ public:
     //==============================================================================
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
+    
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParametersLayout();
+    juce::AudioProcessorValueTreeState apvtsTwo {*this, nullptr, "parameters", createParametersLayout()};
+    
 
 private:
     //==============================================================================
